3714819c81d77e9b1ada9bf9e96589a9151efaa4
jitesh yadav <jiteshkumaryadav99@gmail.com>
2024-02-14 11:14:00 +0530

    change in updatePassword.jsx file

--- src/pages/UpdatePassword.jsx
+++ src/pages/UpdatePassword.jsx
@@ -10,7 +10,7 @@ function UpdatePassword() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const location = useLocation()
-  const { loading } = useSelector((state) => state.auth)
+  const { loading,token } = useSelector((state) => state.auth)
   const [formData, setFormData] = useState({
     password: "",
     confirmPassword: "",
@@ -30,7 +30,7 @@ function UpdatePassword() {
 
   const handleOnSubmit = (e) => {
     e.preventDefault()
-    const token = location.pathname.split("/").at(-1)
+    // const token = location.pathname.split("/").at(-1)
     dispatch(resetPassword(password, confirmPassword, token, navigate))
   }
 
