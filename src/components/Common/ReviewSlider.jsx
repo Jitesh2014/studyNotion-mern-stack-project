105c567b732271b5ed580032d8073f908b7d523a
Jitesh Yadav <121483007+Jitesh2014@users.noreply.github.com>
2024-07-29 12:36:30 +0530

    Update ReviewSlider.jsx
    make responsiveness

--- src/components/Common/ReviewSlider.jsx
+++ src/components/Common/ReviewSlider.jsx
@@ -36,8 +36,8 @@ function ReviewSlider() {
   // console.log(reviews)
 
   return (
-    <div className="text-white">
-      <div className="my-[50px] h-[184px] max-w-maxContentTab lg:max-w-maxContent">
+    <div className="text-white w-[100%] mx-auto">
+      <div className="my-[50px] mx-auto h-[184px] max-w-maxContentTab lg:max-w-maxContent">
         <Swiper
           slidesPerView={4}
           spaceBetween={25}
